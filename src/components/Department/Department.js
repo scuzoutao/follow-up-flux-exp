35c23063405b412ef5b2ccf1f616b6aefd717bdd
Mingdong Luo <mdluo@nsmss.com>
2015-11-18 11:10:39 +0800

    Move request for doctors to Edit

--- src/components/Department/Department.js
+++ src/components/Department/Department.js
@@ -7,7 +7,6 @@ import React, { Component, PropTypes } from 'react';
 
 import * as DepartmentActionCreators from '../../actions/Department/DepartmentActionCreators';
 import DepartmentStore from '../../stores/Department/DepartmentStore';
-import DoctorStore from '../../stores/Department/DoctorStore';
 import connectToStores from '../../decorators/connectToStores';
 
 import Header from '../Layouts/Header';
@@ -22,20 +21,18 @@ function requestData(props) {
   const { params } = props;
   const DepartmentId = parseId(params);
   DepartmentActionCreators.requestDepartment(DepartmentId);
-  DepartmentActionCreators.requestDoctors(DepartmentId);
 }
 
 function getState(props) {
   const id = parseId(props.params);
   const department = DepartmentStore.get();
-  const doctors = DoctorStore.get();
   if (id !== department.id) {
     return {};
   }
-  return { department, doctors };
+  return { department};
 }
 
-@connectToStores([DepartmentStore, DoctorStore], getState)
+@connectToStores([DepartmentStore], getState)
 export default class Department extends Component {
 
   static propTypes = {
@@ -43,13 +40,11 @@ export default class Department extends Component {
       id: PropTypes.string.isRequired
     }).isRequired,
 
-    department: PropTypes.object,
-    doctors: PropTypes.array
+    department: PropTypes.object
   };
 
   static defaultProps = {
-    department: {id: 0, name: '加载中...', description: '加载中...'},
-    doctors: []
+    department: {id: 0, name: '加载中...', description: '加载中...'}
   };
 
   componentWillMount() {
@@ -68,8 +63,7 @@ export default class Department extends Component {
         <Header />
         <Navigation />
         { React.cloneElement(this.props.children, {
-          department: this.props.department,
-          doctors: this.props.doctors
+          department: this.props.department
         }) }
       </div>
     );
--- src/components/Department/Edit/Edit.js
+++ src/components/Department/Edit/Edit.js
@@ -7,6 +7,8 @@ import React, { Component, PropTypes } from 'react';
 import DocumentTitle from 'react-document-title';
 
 import * as DepartmentActionCreators from '../../../actions/Department/DepartmentActionCreators';
+import DoctorStore from '../../../stores/Department/DoctorStore';
+import connectToStores from '../../../decorators/connectToStores';
 
 import Head from './Head'
 import Tab from './Tab'
@@ -14,7 +16,42 @@ import TabBasic from './TabBasic'
 import TabDoctor from './TabDoctor'
 import TabAdmin from './TabAdmin'
 
+function parseId(params) {
+  return parseInt(params.id);
+}
+
+function requestData(props) {
+  const { params } = props;
+  const DepartmentId = parseId(params);
+  DepartmentActionCreators.requestDoctors(DepartmentId);
+}
+
+function getState(props) {
+  const id = parseId(props.params);
+  const doctors = DoctorStore.get();
+  return { doctors };
+}
+
+@connectToStores([DoctorStore], getState)
 export default class Edit extends Component {
+
+  static propTypes = {
+    doctors: PropTypes.array
+  };
+
+  static defaultProps = {
+    doctors: []
+  };
+
+  componentWillMount() {
+    requestData(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.params.id !== this.props.params.id) {
+      requestData(nextProps);
+    }
+  }
 
   render() {
 
