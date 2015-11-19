af4b8251bf24256dac791cee10b89b8ea44a2ec7
Mingdong Luo <mdluo@nsmss.com>
2015-11-19 22:37:13 +0800

    Use Immutable.js as DepartmentStore data set

--- src/stores/Department/DepartmentStore.js
+++ src/stores/Department/DepartmentStore.js
@@ -4,15 +4,16 @@
  */
 
 import assign from 'object-assign';
+import Immutable from 'immutable'
 
 import { register } from '../../dispatcher/AppDispatcher';
 import { createStore } from '../../utils/StoreUtils';
 
-const _department = {};
+var _department = Immutable.Map();
 
 const DepartmentStore = createStore({
-  get(departmentID) {
-    return _department;
+  get() {
+    return _department.toJS();
   }
 });
 
@@ -20,7 +21,7 @@ DepartmentStore.dispatchToken = register(action => {
   if (action.type == 'REQUEST_DEPARTMENT_SUCCESS') {
     const responseDepartment = action.response;
     if (responseDepartment) {
-      assign(_department, responseDepartment);
+      _department = Immutable.Map(responseDepartment);
       DepartmentStore.emitChange();
     }
   }
