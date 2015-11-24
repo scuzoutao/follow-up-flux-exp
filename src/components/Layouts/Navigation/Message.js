74508aa13621cbd24842858c249bb317b3577da1
Mingdong Luo <mdluo@nsmss.com>
2015-11-24 19:12:40 +0800

    Add message event listener

--- src/components/Layouts/Navigation/Message.js
+++ src/components/Layouts/Navigation/Message.js
@@ -5,15 +5,32 @@
 
 import React, { Component } from 'react';
 
+import DoctorStore from '../../../stores/Department/DoctorStore';
+
 require("./Message.scss");
 
 export default class Message extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {show: false};
+    this._onShowMessage = this._onShowMessage.bind(this);
+    this._onHideMessage = this._onHideMessage.bind(this);
+  }
+
+  componentDidMount() {
+    $('.ui.message').transition({
+      animation: 'fly down',
+      duration: '0s'
+    });
+    DoctorStore.addChangeListener(this._onShowMessage);
+  }
 
   render() {
     return (
       <div className="component message">
         <div className="ui warning message">
-          <i className="close icon"></i>
+          <i className="close icon" onClick={this._onHideMessage}></i>
           <div className="header">
             You must register before you can do that!
           </div>
@@ -21,6 +38,21 @@ export default class Message extends Component {
         </div>
     </div>
     )
+  }
+
+  _onShowMessage() {
+    if (!this.state.show) {
+      $('.ui.message').transition('fly down');
+      this.setState({show: true});
+      window.setTimeout(this._onHideMessage, 5000);
+    }
+  }
+
+  _onHideMessage() {
+    if (this.state.show) {
+      $('.ui.message').transition('fly down');
+      this.setState({show: false});
+    }
   }
 
 }
